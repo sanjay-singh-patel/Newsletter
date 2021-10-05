92e88570f0de197c54329d39fa6a9fbac6cbfd94
Sanjay Singh Patel <57282318+sanjay-singh-patel@users.noreply.github.com>
2021-10-05 12:37:07 +0530

    Full Application Done

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from .forms import SubscriberForm, MailMessageForm
 from django.contrib import messages
-
+from django.core.mail import send_mail
+from django_pandas.io import read_frame
+from .models import Subscribers
 # Create your views here.
 
 
@@ -23,8 +25,21 @@ def index(request):
 def mail_letter(request):
     if request.method == 'POST':    
         form = MailMessageForm(request.POST)
+        emails = Subscribers.objects.all()
+        df = read_frame(emails, fieldnames = ['email'])
+        mail_list = df['email'].values.tolist()
+        print(mail_list)
         if form.is_valid():
             form.save()
+            title = form.cleaned_data.get('title')
+            message = form.cleaned_data.get('message')
+            send_mail(
+                title, 
+                message,
+                '',
+                mail_list,
+                fail_silently=False,   
+            )
             messages.success(request,'Mail has been sent')
             return redirect('mail-letter')
     else:
